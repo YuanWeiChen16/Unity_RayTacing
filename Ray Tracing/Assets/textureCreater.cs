f923d8400394285056f7291805c57096ffd57068
YuanWeiChen16 <wficta137>
2020-10-28 17:29:52 +0800

    add first test

--- Ray Tracing/Assets/textureCreater.cs
+++ Ray Tracing/Assets/textureCreater.cs
@@ -5,16 +5,22 @@ using System.IO;
 public class textureCreater : MonoBehaviour
 {
     public Camera ThisCamera;
+    public GameObject ThisLight;
     public int texture_width = 512;
     public int texture_height = 512;
     public int texture_SuperSample = 32;
-    public int texture_boundtime = 4;    
+    public int texture_boundtime = 4;
     Texture2D tex;
+    float[] Bterm = new float[5];
+
     // Start is called before the first frame update
     void Start()
     {
-
-
+        Bterm[0] = 1;
+        Bterm[1] = 0.5f;
+        Bterm[2] = 0.5f * 0.5f;
+        Bterm[3] = 0.5f * 0.5f * 0.5f;
+        Bterm[4] = 0.5f * 0.5f * 0.5f * 0.5f;
     }
 
     // Update is called once per frame
@@ -35,14 +41,13 @@ public class textureCreater : MonoBehaviour
                     }
                     targetColor = targetColor / texture_SuperSample;
                     targetColor.a = 1.0f;
-                    tex.SetPixel(i, j, targetColor );
+                    tex.SetPixel(i, j, targetColor);
                 }
             }
             Debug.Log("Saving....");
             var bytes = tex.EncodeToPNG();
             File.WriteAllBytes(Path.Combine(Application.dataPath, "img.png"), bytes);
         }
-
     }
 
 
@@ -50,57 +55,68 @@ public class textureCreater : MonoBehaviour
     {
         RaycastHit MYRayHit;
         Color color = Color.black;
+        //if (BoundTime == 0)
+        //{
+        //    if (Physics.Raycast(Input, out MYRayHit, 2000f))
+        //    {
+        //        Renderer renderer = MYRayHit.collider.GetComponent<MeshRenderer>();
+        //        if (MYRayHit.collider.tag == "Light")
+        //        {
+        //            return Color.white;
+        //        }
+        //        else
+        //        {
+        //            Vector3 targetDir = MYRayHit.normal.normalized + new Vector3(Random.Range(-1, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;                    
+        //            color = renderer.material.color * RHit(new Ray(MYRayHit.point, targetDir), BoundTime + 1);
+        //        }
+        //        //color = renderer.material.color;
+        //        //}
+        //        return color;
+        //    }
+        //}
+        if (BoundTime >= texture_boundtime)
+        {
+            Vector3 Ldir = ThisLight.GetComponent<Transform>().position - Input.origin;
+            Ray NRay = new Ray(Input.origin, Ldir.normalized);
+            if (Physics.Raycast(NRay, out MYRayHit, 2000f))
+            {
+                if (MYRayHit.collider.tag == "Light")//done
+                {
+                    return Color.white * 3;
+                }
+            }
+            return Color.black;
+        }
         if (Physics.Raycast(Input, out MYRayHit, 2000f) && (BoundTime < texture_boundtime))
         {
             Renderer renderer = MYRayHit.collider.GetComponent<MeshRenderer>();
+            Ray scattered = new Ray();
+            Color attenuation = Color.black;
+            Color emitted = Color.black;
 
-            //Mesh hitMesh = MYRayHit.collider.GetComponent<MeshFilter>().sharedMesh;
-            //int Tri_Idx = MYRayHit.triangleIndex;
-            //int Tri_Idx1 = hitMesh.triangles[Tri_Idx * 3];
-            //int Tri_Idx2 = hitMesh.triangles[Tri_Idx * 3 + 1];
-            //int Tri_Idx3 = hitMesh.triangles[Tri_Idx * 3 + 2];
-            //int materialIdx = -1;
-            //for (int k = 0; k < hitMesh.subMeshCount; k++)
-            //{
-            //    int[] tri = hitMesh.GetTriangles(k);
-            //    for (int tr = 0; tr < tri.Length; tr = tr + 3)
-            //    {
-            //        if ((tri[tr + 0] == Tri_Idx1) && (tri[tr + 1] == Tri_Idx2) && (tri[tr + 2] == Tri_Idx3))
-            //        {
-            //            materialIdx = k;
-            //            break;
-            //        }
-            //    }
-            //    if (materialIdx != -1)
-            //    {
-            //        break;
-            //    }
-            //}
-            //Texture2D texture2D = renderer.materials[materialIdx].mainTexture as Texture2D;
-            //if (texture2D != null)
-            //{
-            //    Vector2 pCoord = MYRayHit.textureCoord;
-            //    pCoord.x *= texture2D.width;
-            //    pCoord.y *= texture2D.height;
-            //    Vector2 tiling = renderer.materials[materialIdx].mainTextureScale;
-            //    color = texture2D.GetPixel(Mathf.FloorToInt(pCoord.x * tiling.x), Mathf.FloorToInt(pCoord.y * tiling.y));
-            //}
-            //else
-            //{
-            if (MYRayHit.collider.tag == "Light")
+            if (MYRayHit.collider.tag == "Light")//done
             {
-                return renderer.material.color;
+
+                return Color.white * 3;
             }
-            else
+            else if (MYRayHit.collider.tag == "lambertian")
             {
-                Vector3 targetDir = MYRayHit.normal.normalized + new Vector3(Random.Range(-1, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+                emitted = Color.black;
+                Vector3 scatter_direction = MYRayHit.normal + new Vector3(Random.Range(-1, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+                scattered = new Ray(MYRayHit.point, scatter_direction);
+                attenuation = renderer.material.color * 0.5f;
+            }
+            else if (MYRayHit.collider.tag == "metal")
+            {
+                emitted = Color.black;
 
-                color = 0.5f * renderer.material.color +  RHit(new Ray(MYRayHit.point, targetDir), BoundTime + 1);
 
             }
-            //color = renderer.material.color;
-            //}
-            return color;
+            else if (MYRayHit.collider.tag == "dielectric ")
+            {
+                emitted = Color.black;
+            }          
+            return emitted + attenuation * RHit(scattered, ++BoundTime);
         }
         else
         {
